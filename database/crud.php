bc0949ea2579024e1c2a8737e36a3a40e43d5820
PedroOssuna <43084813+PedroOssuna@users.noreply.github.com>
2020-03-16 06:15:35 -0400

    Update crud.php

--- database/crud.php
+++ database/crud.php
@@ -2,7 +2,8 @@
 	function insertVeiculo($placa,$marca,$modelo,$ano,$combustivel,$proprietario){
 			include 'conn.php';
 			
-			$sql = "insert into veiculo(placa,marca,modelo,ano,tipo_combustivel, proprietario) values('$placa','$marca','$modelo',$ano,'$combustivel','$proprietario');";
+            $sql = "insert into veiculo(placa,marca,modelo,ano,tipo_combustivel, proprietario) 
+            values('$placa','$marca','$modelo',$ano,'$combustivel','$proprietario');";
 
             if ($conn->query($sql) === TRUE) {
                 echo "Novo registro cadastrado.";
@@ -10,7 +11,17 @@
                 echo "Error: " . $sql . "<br>" . $conn->error;
             }
     }
-    
+
+    function deleteVeiculo(){
+    include 'conn.php';
+    $sql = "delete from veiculo where id=3";
+    if (mysqli_query($conn, $sql)) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . mysqli_error($conn);
+    }
+    }
+
     function insertAbastecimento($id_veiculo,$data,$hora,$tipo_combustivel,$preco_litro,$litros_abastecidos,$km_atual,$nome_posto){
             include 'conn.php';
 
